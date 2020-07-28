63f7479981cfc02dc0befa2d0a6b7a94b85d5e9e
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-28 21:06:21 +0530

    Add files via upload

--- src/dao/CityDAO.java
+++ src/dao/CityDAO.java
@@ -1,1 +1,49 @@
+package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import model.City;
+import utility.ConnectionManager;
+
+public class CityDAO{
+	City city;
+	public City getCityByName(String name) throws Exception {
+		
+		ConnectionManager con = new ConnectionManager();
+		Statement st = con.getConnection().createStatement();
+		
+		ResultSet rs = st.executeQuery("SELECT * from CITY where cityname = "+name+"");
+		System.out.println(rs);
+		
+		while(rs.next()) {
+			Long id = rs.getLong("ID");
+			String city_name = rs.getString("NAME");
+			city = new City(id,city_name);
+			
+		}
+		
+		return city;
+	}
+	
+	public void createCity() throws SQLException, Exception {
+		Long cityId = city.getCityId();
+		String cityName=city.getCityName();
+		
+		ConnectionManager cm = new ConnectionManager();
+		
+		String sql = "insert into CITY(CITY_ID,CITY_NAME)VALUES(?,?)";
+		
+		PreparedStatement st  = ConnectionManager.getConnection().prepareStatement(sql);
+		
+		st.setLong(1, cityId);
+		
+		st.setString(2, cityName);
+		
+		st.executeUpdate();
+		
+		ConnectionManager.getConnection().close();
+	}
+}
--- src/dao/TeamDAO.java
+++ src/dao/TeamDAO.java
@@ -1,1 +1,32 @@
+package dao;
 
+import model.Team;
+import utility.ConnectionManager;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.City;
+
+public class TeamDAO{
+	public void createTeam(Team team) throws SQLException, Exception {
+		
+		System.out.println("Create team dao");
+		String name = team.getName();
+		String coach = team.getCoach();
+		Long cityID = team.getCity().getCityId();
+		
+		ConnectionManager cm = new ConnectionManager();
+		
+		String sql = "insert into  TEAM(NAME,COACH,CITY)VALUES(?,?,?)";
+		PreparedStatement st = ConnectionManager.getConnection().prepareStatement(sql);
+		
+		st.setString(1, name);
+		st.setString(2, coach);
+		st.setLong(3,cityID);
+		
+		st.executeUpdate();
+		ConnectionManager.getConnection().close();
+		
+	}
+}
