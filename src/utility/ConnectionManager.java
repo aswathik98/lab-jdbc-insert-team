288a65a964b7a9aad5760adfd6d3ce8a796399a8
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-28 21:07:18 +0530

    Add files via upload

--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,1 +1,35 @@
+package utility;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionManager{
+	
+	public static Properties loadPropertiesFile() throws Exception {
+		Properties prop = new Properties();	
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close(); 
+		return prop;
+	}
+	
+	public static Connection getConnection() throws Exception {
+		
+		Properties prop = new Properties();
+		prop = loadPropertiesFile();
+		String driver = prop.getProperty("driver");
+		String url = prop.getProperty("url");
+		String username =prop.getProperty("username");
+		String password = prop.getProperty("password");
+		
+		Connection con = null;
+		
+		Class.forName(driver);
+		
+		con= DriverManager.getConnection(url,username,password);
+		return con;
+	}
+}
