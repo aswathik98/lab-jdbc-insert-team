9703faeb78d60c60f9505f8d2b69ea1bab06dca1
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-28 21:05:42 +0530

    Add files via upload

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,54 @@
+package controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+
+import dao.*;
+import model.City;
+import model.Team;
+
+
+public class Main{
+	
+	public static void main(String[] args) throws Exception {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		CityDAO citydao = new CityDAO();
+		TeamDAO teamdao = new TeamDAO();
+		
+		System.out.println("Enter city name :");
+		String cityName = br.readLine();
+		
+		System.out.println("Enter city id :");
+		long cityId =  Long.parseLong(br.readLine());
+		
+		
+		City city = new City(cityId,cityName);
+		
+		System.out.println("Enter team name");
+		String name=br.readLine();
+		
+		System.out.println("Enter coach name");
+		String coachName=br.readLine();
+		
+		Team team = new Team(name, coachName,city);
+		teamdao.createTeam(team);
+		
+		System.out.println("Enter name of city");
+		String cityname = br.readLine();
+		
+		city = citydao.getCityByName(cityname);
+		
+		System.out.println("City id = "+city.getCityId());
+		System.out.println("city name = "+city.getCityName());	
+		
+		
+		
+		
+		
+		
+}
+}
