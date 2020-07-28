745dcef4a26cc035e8a0e529757c3a5de52c8c10
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-28 21:06:50 +0530

    Add files via upload

--- src/model/City.java
+++ src/model/City.java
@@ -1,1 +1,31 @@
+package model;
+ public class City{
+	 private long cityId;
+	 
+	 private String cityName;
 
+	public long getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(long cityId) {
+		this.cityId = cityId;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public City(long cityId, String cityName) {
+	
+		this.cityId = cityId;
+		this.cityName = cityName;
+	}
+	 
+	 
+	 
+ }
